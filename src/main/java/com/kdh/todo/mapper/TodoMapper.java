2a3d57914dfa0788a5b8c1995981e555483a7a3c
강동하 <kang97@douzone.com>
2026-04-02 16:08:18 +0900

    20260402 1608 DB 설정 완료

--- src/main/java/com/kdh/todo/mapper/TodoMapper.java
+++ src/main/java/com/kdh/todo/mapper/TodoMapper.java
@@ -1,19 +1,17 @@
 package com.kdh.todo.mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.kdh.todo.dto.TodoDTO;
 
 @Mapper
 public interface TodoMapper {
-    // 1. 전체 할 일 목록 조회
+    // 1. 전체 목록 조회
     List<TodoDTO> selectTodoList();
     
-    // 2. 목표 등록 (처음 할 일 만들 때)
+    // 2. 목표 등록 (초기값 'N')
     int insertTodo(TodoDTO todo);
 
-    // 3. [핵심] 성과 기록 및 완료 처리 ('Y'로 자동 업데이트)
+    // 3. 성과 기록 (달성률 반영 및 'Y' 업데이트)
     int updateTodoComplete(TodoDTO todo); 
 }
